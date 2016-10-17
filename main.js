c2934eaac119f0c514459597ca025866c443a8bc
sarora6 <sarora6@ncsu.edu>
2016-10-17 00:18:55 -0400

    Update main.js

--- main.js
+++ main.js
@@ -194,9 +194,9 @@ function constraints(filePath)
    console.log(filePath);
    console.log(options);
    var buf = fs.readFileSync(filePath, "utf8");
-   console.log(buf);
+   console.log("buf");
 	var result = esprima.parse(buf, options);
-	console.log(result);
+	console.log("result");
 	traverse(result, function (node) 
 	{
 		if (node.type === 'FunctionDeclaration') 
