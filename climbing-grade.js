bac56266260e4e37c26d9e558521be3921a0daae
Srishti Arora <sarora6@ncsu.edu>
2016-10-18 20:58:56 +0000

    Build Fail - Revert Commit

--- climbing-grade.js
+++ climbing-grade.js
@@ -1,5 +1,5 @@
 var ClimbingGrade = (function() {
-  // Climbing Grade grades
+  // Climbing Grade grades clac
   function ClimbingGrade(grade, system, options) {
     this._inputGrade = grade;
     this._inputSystemName = system;
