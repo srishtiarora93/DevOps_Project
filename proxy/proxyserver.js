81a95c8a1df51e3f5de1c34fb5aadba69b27956c
Chirag Kapadia <ckapadi@ncsu.edu>
2016-11-09 17:22:03 -0500

    proxy server canary disabling on 500

--- proxy/proxyserver.js
+++ proxy/proxyserver.js
@@ -4,16 +4,24 @@ var httpProxy = require('http-proxy')
 var options = {};
 var proxy   = httpProxy.createProxyServer(options);
 
+var targets = ["http://162.243.23.35:50101", "http://162.243.23.35:50100" ];
 
 // Redirect 1/3rds of request to canary
 var counter = 0;
 var proxyServer  = http.createServer(function(req, res)
 {
 	console.log(req.url);
-	if(counter%3 == 0)
-		proxy.web( req, res, {target: "http://162.243.23.35:50101" } );
+	if(counter%3 == 0) 
+		proxy.web( req, res, {target: targets[0] } );
 	else 
-		proxy.web( req, res, {target: "http://162.243.23.35:50100" } );
+		proxy.web( req, res, {target: targets[1] } );
+	
+	// Do not redirect to canary if receive a 500
+	proxy.on('proxyRes', function (proxyRes, req, res) {
+		if(proxyRes.statusCode == 500){
+			targets[0] = targets[1];
+		};
+	});
 	counter++;
 });
 
