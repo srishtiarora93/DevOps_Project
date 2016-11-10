b4de138d6c31890393e08189de4c7238fbbafeb3
Srishti Arora <sarora6@ncsu.edu>
2016-11-10 19:52:00 +0000

    configure redis script

---
+++ start_redis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+wget http://download.redis.io/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+cd redis-stable
+make
+apt-get install redis-tools
+
+cd src
+./redis-server --daemonize yes
+
+redis-cli CONFIG SET protected-mode no
+
+
+
