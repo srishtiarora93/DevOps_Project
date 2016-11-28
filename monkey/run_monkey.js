244ba43ff465221d492aa0c5e2a8881019e61281
Srishti Arora <sarora6@ncsu.edu>
2016-11-28 04:35:55 +0000

    updated monkey

--- monkey/run_monkey.js
+++ monkey/run_monkey.js
@@ -1,13 +1,41 @@
 var exec = require('child_process').exec;
+var sleep = require('sleep')
 
 exec('chmod "+x" diet_monkey.sh');
 
 // Random time interval to run the monkey at.
 var timeInSec = Math.floor(Math.random()*25) + 5;
 
-setTimeout(function(){
-    exec('./diet_monkey.sh', function(err, out, code) {
+exec('./diet_monkey.sh', function(err, out, code) {
         console.log(out);
-    });
-}, timeInSec*1000);
+	sleep.sleep(5);	
+	var stress = require('stress-node');
+	stress({
+    		url: "http://162.243.23.35:50100/convert/french/yds/5.1",
+    		method: 'GET',
+    		timeout: 1000,
+    		amount: 1000,
+    		concurrent: 1000,
+    		data: null,
+		progress: function(report) {
+        		// console.log(report)
+    		}
+	}, function(report) {
+    		console.log(report);
+	});
+	sleep.sleep(10);
 
+        stress({
+                url: "http://162.243.23.35:50100/convert/french/yds/5.1",
+                method: 'GET',
+                timeout: 1000,
+                amount: 1000,
+                concurrent: 1000,
+                data: null,
+                progress: function(report) {
+                        // console.log(report)
+                }
+        }, function(report) {
+                console.log(report);
+        });
+});
